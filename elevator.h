ff1ca6073e8b44c484b49e72c38f050131025fec
Adotac <otacool89@gmail.com>
2022-10-31 15:01:39 +0800

    init

---
+++ elevator.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#ifndef elevator
+#define elevator
+
+#define TRUE 1;
+#define FALSE 0;
+
+
+
+#endif
